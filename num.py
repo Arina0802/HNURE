99f9ad7b51256e509be6ef965c2df12845658ae9
Arina0802 <arinavish0802@gmail.com>
2025-11-14 16:19:16 +0200

    Add files via upload

---
+++ num.py
@@ -0,0 +1,15 @@
+num1 = int (input("num 1:"))
+num2 = int (input("num 2:"))
+operasion = input("selekt operasion(-,+,*,/):")
+if operasion == '+':
+    result = (num1 + num2)
+    print("Result:", result)
+if operasion == '-':
+    result = (num1 - num2)
+    print("Result:", result)
+if operasion == '*':
+    result = (num1 * num2)
+    print("Result:", result)
+if operasion == '/':
+    result = (num1 / num2)
+    print("Result:", result)
